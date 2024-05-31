92e782125b3e549dcc9217d739c041dddd25fe30
mariabreux <104687787+mariabreux@users.noreply.github.com>
2024-05-31 15:53:13 +0200

    Aed details and login

--- app/src/main/java/com/finalproject/aedvenice/ui/screens/AedDetailsScreen.kt
+++ app/src/main/java/com/finalproject/aedvenice/ui/screens/AedDetailsScreen.kt
@@ -1,7 +1,43 @@
 package com.finalproject.aedvenice.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.finalproject.aedvenice.ui.theme.BorderPink
+import com.finalproject.aedvenice.ui.theme.DarkPink
+import com.finalproject.aedvenice.ui.theme.LightPink
 
 @Composable
 fun AedDetailsScreen(
@@ -9,15 +45,153 @@ fun AedDetailsScreen(
     onConfirm: () -> Unit
 ) {
 
+    Dialog(onDismissRequest = onDismiss) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .height(415.dp)
+                .width(300.dp)
+                .background(LightPink)
+                .border(1.dp, color = DarkPink, shape = RoundedCornerShape(5.dp))
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.Top,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                Text(
+                    text = "Name of the building",
+                    style = MaterialTheme.typography.labelLarge.copy(fontWeight = FontWeight.SemiBold),
+                )
+
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Column(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 25.dp)
+            ) {
+                Text(
+                    text = "Address: " /* TODO: + viewModel.getAedAdress*/,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold)
+                )
+
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                Text(
+                    text = "Timetable:",
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold)
+                )
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Column(
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(10.dp))
+                    .background(Color.White)
+                    .height(95.dp)
+                    .width(200.dp)
+
+            ) {
+                Row {
+                    //TODO: Rows with the actual values
+                    Text(
+                        text = "Mon - Wed",
+                        fontSize = 10.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(top = 10.dp)
+
+
+                    )
+                    Divider(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .width(1.dp),
+                        color = Color.LightGray
+                    )
+
+                    Text(
+                        text = "9am - 6pm",
+                        fontSize = 10.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(top = 10.dp)
+                    )
+
+                }
+                Divider(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(1.dp),
+                    color = Color.LightGray
+                )
+
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Column(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 25.dp)
+            ) {
+                Text(
+                    text = "Notes: " /* TODO: + viewModel.getAedNotes*/,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold)
+                )
+
+            }
+
+            Spacer(modifier = Modifier.height(55.dp))
+
+
+            Button(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = DarkPink,
+                ),
+                border = BorderStroke(2.dp, BorderPink),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    text = "Go to location",
+                    color = Color.White,
+                    style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Bold)
+                )
+            }
+            Spacer(modifier = Modifier.padding(3.dp))
+            Text(
+                text = "Report problem",
+                style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.SemiBold),
+                color = DarkPink,
+                textDecoration = TextDecoration.Underline,
+
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        //TODO
+                    },
+                textAlign = TextAlign.Center
+            )
+        }
+
+    }
+
 }
-
-
 
 
 @Preview(showBackground = true)
 @Composable
-fun AedDetailScreenPreview(){
+fun AedDetailScreenPreview() {
     AedDetailsScreen(onDismiss = { /*TODO*/ }) {
-        
+
     }
 }
